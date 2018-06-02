176aa32d4dafb05e2953d656c3664ce42d19a402
Tim Book <timbook@ga.fios-router.home>
2018-06-02 01:48:15 -0400

    added standard normal, exceptions

--- normal.py
+++ normal.py
@@ -22,13 +22,11 @@ class Normal:
 
     def __add__(self, Y):
         if isinstance(Y, Normal):
-            new_mu = self.mu + Y.mu
-            new_sigma = (self.var + Y.var)**0.5
             return Normal(new_mu, new_sigma)
         elif isinstance(Y, (int, float)):
             return Normal(self.mu + Y, self.sigma)
         else:
-            raise TypeError
+            raise TypeError("Only addition by another (independent) Normal, int, or float supported.")
 
     def __radd__(self, c):
         if isinstance(c, (int, float)):
@@ -36,12 +34,25 @@ class Normal:
 
     def __mul__(self, c):
         if isinstance(c, (int, float)):
-            return Normal(c * self.mu, c * self.sigma)
+            self.mu *= c
+            self.sigma *= c
         else:
-            raise TypeError
+            raise TypeError("Only multiplicated by int or float supported.")
 
     def __rmul__(self, c):
         return self.__mul__(c)
+
+    def __truediv__(self, c):
+        if c != 0:
+            return self.__mul__(1 / c)
+        else:
+            raise ZeroDivisionError("Cannot divide a Normal by zero!")
+
+    def __neg__(self):
+        return Normal(-self.mu, self.sigma)
+
+    def __pos__(self):
+        return self
 
     def sample(self, *shape):
         return np.random.randn(*shape)
@@ -50,7 +61,9 @@ class Normal:
         return np.exp(t*self.mu + 0.5*(t**2)*(self.var))
 
     def pdf(self, x):
-        return self.sp.pdf(x)
+        coef = 1 / (2 * np.pi * self.var)**0.5
+        pow = -0.5 * (x - self.mu)**2 / self.var
+        return coef * np.exp(pow)
 
     def cdf(self, x):
         return self.sp.cdf(x)
@@ -58,9 +71,29 @@ class Normal:
     def prob_interval(self, a, b):
         return self.cdf(b) - self.cdf(a)
 
-    # TODOs:
+    def to_standard(self):
+        if self.mu == 0 and self.sigma == 1:
+            return StandardNormal()
+        else:
+            raise ValueError("Must be Normal(0, 1) to standardize!")
+
+    # TODO:
     # - __iadd__, __imul__, __truediv__, ...
     # __or__ for Bayes priors?
     # __pow__(self, 2) --> ChiSq()
     # __truediv__ --> Cauchy()
     # Remove scipy dependency
+
+class StandardNormal(Normal):
+    def __init__(self):
+        super().__init__(0, 1)
+
+    def __repr__(self):
+        return f"StandardNormal(mu=0, sigma=1)"
+
+    def to_nonstandard(self, mu, sigma):
+        return Normal(mu=mu, sigma=sigma)
+
+    # TODO: __pow__(self, 2) --> ChiSq()
+
+
