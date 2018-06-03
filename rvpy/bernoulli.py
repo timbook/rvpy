53cacec3c26154c491d1369cca240e225b700825
tim <tim@yoga.fios-router.home>
2018-06-03 01:15:52 -0400

    added binomial, simplified bernoulli

--- rvpy/bernoulli.py
+++ rvpy/bernoulli.py
@@ -1,52 +1,11 @@
-import numpy as np
+from binomial import Binomial
 
-class Bernoulli:
+class Bernoulli(Binomial):
     def __init__(self, p):
-        assert isinstance(p, float) and p < 1 and p > 0, "p must be a float between 0 and 1"
-        # Parameters
-        q = 1 - p
-        self.p = p
-        self.q = q
-
-        # Moments
-        self.std = (p*q)**0.5
-        self.mean = p
-        self.var = p*q
-        self.skew = (1 - 2*p) / np.sqrt(p*q)
-        self.kurtosis = (1 - 6*p*q) / (p*q) 
-
-        # Other properties
-        # self.median = _
-        # self.mode = _
-        # self.entropy = _
+        super().__init__(n=1, p=p)
 
     def __repr__(self):
         return f"Bernoulli(p={self.p})"
 
-    def __pos__(self):
-        return self
-
-    # TODO: __add__ to Binomials
-
-    def sample(self, *shape):
-        return np.random.binomial(n=1, p=self.p, size=shape)
-
-    def pdf(self, x):
-        assert x in [0, 1], "Bernoulli has support {0, 1}"
-        return x*self.p + (1 - x)*self.q
-
-    def cdf(self, x):
-        if x < 0:
-            return 0
-        elif x < 1:
-            return self.q
-        else:
-            return 1
-
-    def mgf(self, t):
-        return self.q + self.p * np.exp(t)
-
     def to_binomial(self):
-        # TODO: this
-        # return Binomial(n=1, p=self.p)
-        pass
+        return Binomial(n=1, p=self.p)
---
+++ rvpy/binomial.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+class Binomial:
+    def __init__(self, n, p):
+        assert p < 1 and p > 0, "p must be a float between 0 and 1"
+        assert isinstance(p, float), "p must be a float"
+        assert isinstance(n, int), "n must be an integer"
+        # Parameters
+        q = 1 - p
+        self.p = p
+        self.q = q
+        self.n = n
+
+        # Moments
+        self.mean = n*p
+        self.std = (n*p*q)**0.5
+        self.var = n*p*q
+        self.skew = (1 - 2*p) / np.sqrt(n*p*q)
+        self.kurtosis = (1 - 6*p*q) / (n*p*q) 
+
+    def __repr__(self):
+        return f"Binomial(n={self.n}, p={self.p})"
+
+    def __pos__(self):
+        return self
+
+    def __add__(self, Y):
+        assert isinstance(Y, Binomial), "can only add Binomials to Binomials"
+        assert self.p == Y.p, "values of p must match in order to add"
+        return Binomial(self.n + Y.n, self.p)
+
+    def sample(self, *shape):
+        return np.random.binomial(n=self.n, p=self.p, size=shape)
+
+    def pdf(self, k):
+        assert k >= 0 and k <= self.n, "k must be between 0 and n"
+        assert isinstance(k, int), "k must be an integer"
+        f = np.math.factorial
+        coef = f(self.n) / (f(k) * f(self.n - k))
+        return coef * (self.p**k) * (self.q)**(self.n - k)
+
+    def cdf(self, k):
+        assert isinstance(k, int), "k must be an integer"
+        if k < 0:
+            return 0
+        elif k <= self.n:
+            return sum(self.pdf(i) for i in range(k + 1))
+        else:
+            return 1
+
+    def mgf(self, t):
+        return (self.q + self.p * np.exp(t))**self.n
