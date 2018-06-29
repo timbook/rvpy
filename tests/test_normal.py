b27544932e0e5d23335ae5cca9ef090bf89ab470
Tim Book <timbook@ga.attlocal.net>
2018-06-29 00:21:57 -0500

    started unittesting for normal module

---
+++ tests/test_normal.py
@@ -0,0 +1,48 @@
+import unittest
+import sys
+import random
+
+sys.path.append('..')
+
+import rvpy
+
+class NormalTests(unittest.TestCase):
+    def setUp(self):
+        self.rnd1 = random.random()
+        self.rnd2 = random.random()
+        self.Z = rvpy.StandardNormal()
+        self.N1 = rvpy.Normal(mu=self.rnd1, sigma=self.rnd2)
+
+    def test_normal_moments(self):
+        self.assertEqual(self.Z.mean, 0)
+        self.assertEqual(self.Z.std, 1)
+        self.assertEqual(self.Z.var, 1)
+        self.assertEqual(self.N1.mean, self.rnd1)
+        self.assertEqual(self.N1.std, self.rnd2)
+        self.assertEqual(self.N1.var, self.rnd2**2)
+
+    def test_normal_pdf(self):
+        self.assertAlmostEqual(self.Z.pdf(10), 0)
+        self.assertAlmostEqual(self.Z.pdf(-10), 0)
+        self.assertAlmostEqual(self.N1.pdf(10), 0)
+        self.assertAlmostEqual(self.N1.pdf(-10), 0)
+
+    def test_normal_cdf(self):
+        self.assertAlmostEqual(self.Z.cdf(10), 1)
+        self.assertAlmostEqual(self.Z.cdf(-10), 0)
+        self.assertAlmostEqual(self.Z.cdf(0), 0.5)
+
+        self.assertAlmostEqual(self.N1.cdf(10), 1)
+        self.assertAlmostEqual(self.N1.cdf(-10), 0)
+        self.assertAlmostEqual(self.N1.cdf(self.rnd1), 0.5)
+
+    def test_normal_conversion(self):
+        X = rvpy.Normal(0, 1)
+        self.assertIsInstance(self.Z.to_nonstandard(), rvpy.Normal)
+        self.assertIsInstance(X.to_standard(), rvpy.StandardNormal)
+
+        with self.assertRaises(ValueError):
+            self.N1.to_standard()
+
+
+
