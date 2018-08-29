2ab6a168b9584d3680380274a883f0be5a3ed7a1
Tim Book <timothykbook@gmail.com>
2018-08-29 00:32:24 -0400

    renamed normal test names

--- tests/test_normal.py
+++ tests/test_normal.py
@@ -11,15 +11,15 @@ class NormalTests(unittest.TestCase):
         self.rnd1 = random.random()
         self.rnd2 = random.random()
         self.Z = rvpy.StandardNormal()
-        self.N1 = rvpy.Normal(mu=self.rnd1, sigma=self.rnd2)
+        self.X = rvpy.Normal(mu=self.rnd1, sigma=self.rnd2)
 
     def test_normal_moments(self):
         self.assertEqual(self.Z.mean, 0)
         self.assertEqual(self.Z.std, 1)
         self.assertEqual(self.Z.var, 1)
-        self.assertEqual(self.N1.mean, self.rnd1)
-        self.assertEqual(self.N1.std, self.rnd2)
-        self.assertEqual(self.N1.var, self.rnd2**2)
+        self.assertEqual(self.X.mean, self.rnd1)
+        self.assertEqual(self.X.std, self.rnd2)
+        self.assertEqual(self.X.var, self.rnd2**2)
 
         print("Unresolved TODOs!")
         # TODO: Higher moments?
@@ -27,13 +27,13 @@ class NormalTests(unittest.TestCase):
     def test_normal_pdf(self):
         self.assertAlmostEqual(self.Z.pdf(10), 0)
         self.assertAlmostEqual(self.Z.pdf(-10), 0)
-        self.assertAlmostEqual(self.N1.pdf(10), 0)
-        self.assertAlmostEqual(self.N1.pdf(-10), 0)
+        self.assertAlmostEqual(self.X.pdf(10), 0)
+        self.assertAlmostEqual(self.X.pdf(-10), 0)
 
         c = random.random()
         self.assertAlmostEqual(
-            self.N1.pdf(self.N1.mean - c),
-            self.N1.pdf(self.N1.mean + c)
+            self.X.pdf(self.X.mean - c),
+            self.X.pdf(self.X.mean + c)
         )
 
         self.assertAlmostEqual(
@@ -45,27 +45,27 @@ class NormalTests(unittest.TestCase):
         self.assertAlmostEqual(self.Z.cdf(10), 1)
         self.assertAlmostEqual(self.Z.cdf(-10), 0)
         self.assertAlmostEqual(self.Z.cdf(0), 0.5)
-        self.assertAlmostEqual(self.N1.cdf(10), 1)
-        self.assertAlmostEqual(self.N1.cdf(-10), 0)
-        self.assertAlmostEqual(self.N1.cdf(self.rnd1), 0.5)
+        self.assertAlmostEqual(self.X.cdf(10), 1)
+        self.assertAlmostEqual(self.X.cdf(-10), 0)
+        self.assertAlmostEqual(self.X.cdf(self.rnd1), 0.5)
 
     def test_normal_conversion(self):
         X = rvpy.Normal(0, 1)
         self.assertIsInstance(self.Z.to_nonstandard(), rvpy.Normal)
         self.assertIsInstance(X.to_standard(), rvpy.StandardNormal)
         with self.assertRaises(ValueError):
-            self.N1.to_standard()
+            self.X.to_standard()
 
     def test_normal_add_sub(self):
-        N1 = self.N1
+        X = self.X
         m = random.random()
         s = random.random()
-        N2 = rvpy.Normal(mu=m, sigma=s)
-        N3 = N1 + N2
-        new_mu = N1.mu + m
-        new_sigma = (N1.sigma**2 + s**2)**0.5
-        self.assertAlmostEqual(N3.mean, new_mu)
-        self.assertAlmostEqual(N3.sigma, new_sigma)
+        X2 = rvpy.Normal(mu=m, sigma=s)
+        X3 = X + X2
+        new_mu = X.mu + m
+        new_sigma = (X.sigma**2 + s**2)**0.5
+        self.assertAlmostEqual(X3.mean, new_mu)
+        self.assertAlmostEqual(X3.sigma, new_sigma)
 
         # TODO: Subtraction tests
         print("Unresolved TODOs!")
