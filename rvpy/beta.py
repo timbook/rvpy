065540dd211c7df4e45bda26bce7c0ab7d7161b2
Tim Book <timothykbook@gmail.com>
2018-11-10 22:56:29 -0500

    beta docs

--- rvpy/beta.py
+++ rvpy/beta.py
@@ -4,7 +4,37 @@ from . import distribution
 from . import cuniform
 
 class Beta(distribution.Distribution):
+    """
+    Beta Distribution using the following parameterization:
+
+    f(x | alpha, beta) = 1 / B(alpha, beta) * x**(alpha - 1) * x**(beta - 1)
+
+    Parameters
+    ----------
+    alpha : float, positive
+        Shape parameter
+    beta : float, positive
+        Shape parameter
+
+    Methods
+    -------
+    to_cuniform()
+        Converts self to CUniform if (alpha, beta) == (1, 1)
+
+    Relationships
+    -------------
+    Let X be Beta(1, 1). Then:
+    * X is CUniform(0, 1)
+    """
     def __init__(self, alpha, beta):
+        """
+        Parameters
+        ----------
+        alpha : float, positive
+            Shape parameter
+        beta : float, positive
+            Shape parameter
+        """
         assert alpha > 0 and beta > 0, "alpha and beta must be positive"
 
         # Parameters
