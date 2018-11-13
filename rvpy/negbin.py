0bbe78f26252f1c504336c49010f89d5ad5308c3
Tim Book <timothykbook@gmail.com>
2018-11-12 20:13:37 -0500

    negbin docs

--- rvpy/negbin.py
+++ rvpy/negbin.py
@@ -3,7 +3,37 @@ from scipy.stats import nbinom
 from . import distribution
 
 class NegativeBinomial(distribution.Distribution):
+    """
+    Negative Binomial Distribution using the following parameterization:
+
+    f(x | r, p) = (x+r-1 r-1) p**n * (1 - p)**x
+
+    Parameters
+    ----------
+    r : integer, positive
+        Number of successes
+    p : float, 0 < p < 1
+        Probability of success
+
+    Methods
+    -------
+    to_geometric()
+        Converts self to Geometric if r == 1
+
+    Relationships
+    -------------
+    Let X, Y be NegativeBinomial. Then:
+    * X + Y is NegativeBinomial
+    """
     def __init__(self, r, p):
+        """
+        Parameters
+        ----------
+        r : integer, positive
+            Number of successes
+        p : float, 0 < p < 1
+            Probability of success
+        """
         assert isinstance(r, int) and r > 0, 'r must be positive integer'
         assert p > 0 and p < 1, 'p must be a number between 0 and 1'
 
@@ -32,7 +62,33 @@ class NegativeBinomial(distribution.Distribution):
         return Geometric(p=self.p)
 
 class Geometric(NegativeBinomial):
+    """
+    Geometric Distribution using the following parameterization:
+
+    f(x | p) = p * (1 - p)**x
+
+    Parameters
+    ----------
+    p : float, 0 < p < 1
+        Probability of success
+
+    Methods
+    -------
+    to_negative_binomial()
+        Converts self to NegativeBinomial
+
+    Relationships
+    -------------
+    Let X, Y be Geometric. Then:
+    * X + Y is NegativeBinomial
+    """
     def __init__(self, p):
+        """
+        Parameters
+        ----------
+        p : float, 0 < p < 1
+            Probability of success
+        """
         super().__init__(r=1, p=p)
 
     def __repr__(self):
